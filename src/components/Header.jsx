8052c6ccbfb9de94e799f09144752d5c50d861bd
Pedro Trovo <trovopedro5@gmail.com>
2026-05-19 01:11:25 -0300

    feat: substituir logo e adicionar favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <NavLink to="/" className="logo-link">
-        <img src="/svg/logo.svg" alt="Pedro Trovo" width={48} height={48} />
+        <img src="/svg/logo_portfolio_official.svg" alt="Pedro Trovo" width={48} height={48} />
       </NavLink>
       <nav>
         <NavLink to="/" end>Home</NavLink>

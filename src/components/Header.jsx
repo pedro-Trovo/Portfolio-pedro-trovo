6fda038251a1bb662ddfca39a445b5d7181e4c9e
Pedro Trovo <trovopedro5@gmail.com>
2026-05-27 22:34:06 -0300

    fix: translate header navigation labels using i18n

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,21 @@
-import { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { useLanguage } from '../i18n'
 import ThemeToggle from './ThemeToggle'
 import LanguageToggle from './LanguageToggle'
 
 function Header() {
-  const [scrolled, setScrolled] = useState(false)
-
-  useEffect(() => {
-    const onScroll = () => setScrolled(window.scrollY > 10)
-    window.addEventListener('scroll', onScroll, { passive: true })
-    return () => window.removeEventListener('scroll', onScroll)
-  }, [])
+  const { t } = useLanguage()
 
   return (
-    <header className={`header${scrolled ? ' header--scrolled' : ''}`}>
+    <header className="header">
       <NavLink to="/" className="logo-link">
         <img src="/svg/Pedro_Trovo_Portfolio_logo.svg" alt="Pedro Trovo" width={40} height={40} />
       </NavLink>
       <nav>
-        <NavLink to="/" end>Home</NavLink>
-        <NavLink to="/projetos">Projetos</NavLink>
-        <NavLink to="/experiencias">Experiências</NavLink>
-        <NavLink to="/contato">Contato</NavLink>
+        <NavLink to="/" end>{t('nav.home')}</NavLink>
+        <NavLink to="/projetos">{t('nav.projects')}</NavLink>
+        <NavLink to="/experiencias">{t('nav.experiences')}</NavLink>
+        <NavLink to="/contato">{t('nav.contact')}</NavLink>
       </nav>
       <div className="header-actions">
         <LanguageToggle />

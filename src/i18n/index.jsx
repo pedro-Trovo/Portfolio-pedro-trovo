76096859b69f5a81a43e3cf8d591450cf91356ec
Pedro Trovo <trovopedro5@gmail.com>
2026-05-27 21:50:00 -0300

    fix: use flat key lookup in i18n t() function instead of nested object traversal

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -20,16 +20,7 @@ export function LanguageProvider({ children }) {
   }
 
   const t = (key) => {
-    const keys = key.split('.')
-    let value = translations[language]
-    for (const k of keys) {
-      if (value && typeof value === 'object' && k in value) {
-        value = value[k]
-      } else {
-        return key
-      }
-    }
-    return value
+    return translations[language]?.[key] ?? key
   }
 
   return (
